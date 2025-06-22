f185abb53a8bddf146ab7c9849cb2506570b0084
developersayem <developersayem012@gmail.com>
2025-06-22 16:42:34 +0600

    database contection added

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import {app} from "./app.ts"
 import dotenv from "dotenv"
+import connectDB from "./db/index.ts"
 
 dotenv.config({
     path:"./.env"
@@ -8,6 +9,14 @@ dotenv.config({
 
 const PORT = process.env.PORT || 5001
 
-    app.listen(PORT,()=>{
-        console.log(`server is running on port ${PORT}`)
-    })
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`\n✅ Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("❌ Failed to connect to the database:", error);
+    process.exit(1); // Exit the process with failure
+}
+);
